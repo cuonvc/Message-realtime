4e5edf2d239872aba08479c2e90d9068c9992523
cuonvc <cuong.fithau@gmail.com>
2023-02-27 22:50:25 +0700

    Init project

---
+++ src/main/java/com/project/message/messagerealtime/MessageRealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.project.message.messagerealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageRealtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageRealtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/message/messagerealtime/MessageRealtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.message.messagerealtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageRealtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
